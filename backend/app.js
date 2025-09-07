7bd206bbac8ea345026c70ce2efeda02474e4baf
Emmanuel Oppong <juniorpara1414@gmail.com>
2025-09-07 06:51:03 +0000

    build

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static frontend
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.error('MongoDB connection error:', err));
